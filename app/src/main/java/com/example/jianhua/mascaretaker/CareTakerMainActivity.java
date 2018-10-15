ffdb8ab8ff63cdd2b519ac750484ce10dcc693c5
Yixiao Chai <ychai37@gatech.edu>
2018-10-14 20:56:42 -0400

    ui  part of creating associate accounts

--- app/src/main/java/com/example/jianhua/mascaretaker/CareTakerMainActivity.java
+++ app/src/main/java/com/example/jianhua/mascaretaker/CareTakerMainActivity.java
@@ -1,7 +1,19 @@
 package com.example.jianhua.mascaretaker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class CareTakerMainActivity extends AppCompatActivity {
 
@@ -9,5 +21,15 @@ public class CareTakerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_care_taker_main);
+
+        Button mCreateAssociateAcountButton = (Button)findViewById(R.id.create_associate_account_button);
+        mCreateAssociateAcountButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createAssociateIntent = new Intent(CareTakerMainActivity.this, CreateAssociateAccountActivity.class);
+                startActivity(createAssociateIntent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/jianhua/mascaretaker/CreateAssociateAccountActivity.java
@@ -0,0 +1,18 @@
+package com.example.jianhua.mascaretaker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Chai on 2018/10/14.
+ */
+
+public class CreateAssociateAccountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_associate_account);
+    }
+
+}
+
