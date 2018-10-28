bd9cb2b62d207fe432ed76d8324ba296b3b79e20
jianhuagatech <huajian.gatech@gmail.com>
2018-10-28 08:28:09 -0400

    fixed untracked files

--- app/src/main/java/com/example/jianhua/mascaretaker/MainActivity.java
+++ app/src/main/java/com/example/jianhua/mascaretaker/MainActivity.java
@@ -6,11 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
+
 public class MainActivity extends AppCompatActivity {
+    private PublisherAdView mPublisherAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPublisherAdView = findViewById(R.id.publisherAdView);
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
+        mPublisherAdView.loadAd(adRequest);
+
         final Button careTakerButton = (Button) findViewById(R.id.careTakerButton);
         careTakerButton.setOnClickListener(new View.OnClickListener() {
             @Override
