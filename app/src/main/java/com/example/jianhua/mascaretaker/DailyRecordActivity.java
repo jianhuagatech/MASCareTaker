83b0916b4bc7be93393f1b9a30b6b9eacdbb8ecf
Andrew Eu <aeu6@gatech.edu>
2018-11-17 11:59:08 -0500

    Updated for more nutrients

--- app/src/main/java/com/example/jianhua/mascaretaker/DailyRecordActivity.java
+++ app/src/main/java/com/example/jianhua/mascaretaker/DailyRecordActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -53,7 +54,9 @@ public class DailyRecordActivity extends ListActivity {
     ArrayAdapter<String> listDataAdapter = null;
     Map<String, Food> map = new HashMap<>();
     DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy-HH-mm-ss-S");
+
     Long last_calories = null; // need this to handle entering food, weird bug with timestamp change, cals null originally
+    double[] lastNutrientList = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +78,8 @@ public class DailyRecordActivity extends ListActivity {
         username = currentUser.getEmail().split("@")[0];
 
         //FOR FRESH ACCOUNTS UNCOMMENT
-//        Food addedFood = new Food("Potato", 80);
+//        Food addedFood = new Food("Potato", 80, 1.1, 1.1, 1.1,
+//                1.1, 1.1, 1.1, 1.1, 1.1, 1.1);
 //        writeFood(addedFood);
 
         listDataAdapter = new ArrayAdapter<String>(this, R.layout.activity_daily_record_row, R.id.listRowTextView, listData);
@@ -107,7 +111,40 @@ public class DailyRecordActivity extends ListActivity {
 
                         String label = timestampArr[3] + ":" + timestampArr[4] + "    " + query; // 5 spaces
 
-                        Food food = new Food(query, calories);
+
+                        String[] nutrientLabels = {"fat", "sat_fat", "cholesterol", "sodium",
+                        "carbs", "fiber", "sugar", "protein", "potassium"};
+
+                        double[] nutrientList = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+
+                        if (data.child("potassium").getValue() == null) { // need to check last item due to async
+                            nutrientList = lastNutrientList;
+                        }
+                        else{
+                            for(int i=0; i < nutrientList.length; i++){
+                                if (data.child( nutrientLabels[i] ).getValue() instanceof Long) {
+                                    Long nutrientLong = (Long) data.child( nutrientLabels[i] ).getValue() ;
+                                    nutrientList[i] = nutrientLong.doubleValue();
+                                }
+                                else {
+                                    nutrientList[i] =(double) data.child( nutrientLabels[i] ).getValue();
+                                }
+
+                            }
+                        }
+
+//                        Double sat_fat = (Double) data.child("sat_fat").getValue();
+//                        Double cholesterol = (Double) data.child("cholesterol").getValue();
+//                        Double sodium = (Double) data.child("sodium").getValue();
+//                        Double carbs = (Double) data.child("carbs").getValue();
+//                        Double fiber = (Double) data.child("fiber").getValue();
+//                        Double sugar = (Double) data.child("sugar").getValue();
+//                        Double protein = (Double) data.child("protein").getValue();
+//                        Double potassium = (Double) data.child("potassium").getValue();
+
+                        Food food = new Food(query, calories, nutrientList[0], nutrientList[1],
+                                nutrientList[2],nutrientList[3],nutrientList[4],nutrientList[5],
+                                nutrientList[6],nutrientList[7],nutrientList[8]);
                         map.put(label, food);
                         listData.add(label);
                     }
@@ -127,7 +164,15 @@ public class DailyRecordActivity extends ListActivity {
         myRef.child(username).child(timestamp).child("query").setValue(food.query);
         myRef.child(username).child(timestamp).child("calories").setValue(food.calories);
 
-        last_calories = new Long(food.calories);
+        myRef.child(username).child(timestamp).child("fat").setValue(food.fat);
+        myRef.child(username).child(timestamp).child("sat_fat").setValue(food.sat_fat);
+        myRef.child(username).child(timestamp).child("cholesterol").setValue(food.cholesterol);
+        myRef.child(username).child(timestamp).child("sodium").setValue(food.sodium);
+        myRef.child(username).child(timestamp).child("carbs").setValue(food.carbs);
+        myRef.child(username).child(timestamp).child("fiber").setValue(food.fiber);
+        myRef.child(username).child(timestamp).child("sugar").setValue(food.sugar);
+        myRef.child(username).child(timestamp).child("protein").setValue(food.protein);
+        myRef.child(username).child(timestamp).child("potassium").setValue(food.potassium);
     }
 
     // When user click list item, this method will be invoked.
@@ -142,7 +187,25 @@ public class DailyRecordActivity extends ListActivity {
         // Create an AlertDialog to show.
         AlertDialog alertDialog = new AlertDialog.Builder(this).create();
         Food food = map.get(itemText);
-        alertDialog.setMessage(food.calories + " calories");
+
+        String msg = "Nutrition Information for " + food.query + "\n\n";
+        msg += "Calories: " + food.calories + "\n\n";
+
+        msg += "Total Fat: " + food.fat + "g" + "\n";
+        msg += "Saturated Fat: " + food.sat_fat + "g" + "\n\n";
+
+        msg += "Cholesterol: " + food.cholesterol + "mg" + "\n\n";
+        msg += "Sodium: " + food.sodium + "mg" + "\n\n";
+
+        msg += "Total Carbohydrates: " + food.carbs + "g" + "\n";
+        msg += "Dietary Fiber: " + food.fiber + "g" + "\n";
+        msg += "Sugar: " + food.sugar + "g" + "\n\n";
+
+        msg += "Protein: " + food.protein + "g" + "\n\n";
+        msg += "Potassium: " + food.potassium + "mg";
+
+        alertDialog.setMessage(msg);
+
         alertDialog.show();
     }
 
@@ -159,7 +222,15 @@ public class DailyRecordActivity extends ListActivity {
             //query = "1 cup sugar, 2 cups lettuce";
             Food newFood = addNewRecord(query);
             // add to firebase storage
-            writeFood(newFood);
+            if (newFood != null) {
+                writeFood(newFood);
+            }
+            else {
+                AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+                alertDialog.setMessage("Invalid query, please try again!");
+                alertDialog.show();
+            }
+
         }
     }
 
@@ -203,14 +274,26 @@ public class DailyRecordActivity extends ListActivity {
             JSONArray foods = jsonObject.getJSONArray("foods");
 
             int totalCalories = 0;
+            double[] nutrientTotalList = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+            String[] nutrientLabels = {"nf_total_fat", "nf_saturated_fat", "nf_cholesterol",
+            "nf_sodium", "nf_total_carbohydrate", "nf_dietary_fiber", "nf_sugars",
+            "nf_protein", "nf_potassium"};
 
             for(int i = 0; i < foods.length(); i++) {
                 JSONObject food = foods.getJSONObject(i);
                 totalCalories = totalCalories + food.getInt("nf_calories");
+
+                for(int j=0; j < nutrientTotalList.length; j++){
+                    nutrientTotalList[j] = nutrientTotalList[j] + food.getDouble( nutrientLabels[j] );
+                }
+
             }
 
             // food= query + calories
-            Food newFood = new Food(query, totalCalories);
+            Food newFood = new Food(query, totalCalories, nutrientTotalList[0], nutrientTotalList[1],
+                    nutrientTotalList[2],nutrientTotalList[3],nutrientTotalList[4],
+                    nutrientTotalList[5],nutrientTotalList[6],nutrientTotalList[7],
+                    nutrientTotalList[8]);
 
 //            String message = String.format("Added %s to firebase storage. Meal contains %s calories.",
 //                    query, Integer.toString(totalCalories));
@@ -219,14 +302,19 @@ public class DailyRecordActivity extends ListActivity {
 //            alertDialog.setMessage(message);
 //            alertDialog.show();
 
+            last_calories = new Long(newFood.calories);
+            lastNutrientList = nutrientTotalList;
+
             return newFood;
 
         } catch (IOException | JSONException e) {
-            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-            alertDialog.setMessage("IO/JSON Exception");
-            alertDialog.show();
+//            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+//            alertDialog.setMessage("IO/JSON Exception");
+//            alertDialog.show();
+            Log.w("============IO ERROR", query);
+
         }
-            return new Food();
+            return null;
     }
 
 }
